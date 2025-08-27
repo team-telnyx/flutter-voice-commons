239ad4e77b5d16fc08cce2214a26c71a914e4b67
Oliver Zimmerman <oezimmerman@gmail.com>
2025-08-27 11:14:05 +0100

    fix: [temp] override flutter_webrtc dependency until 1.0.0 and higher is fixed

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -10,17 +10,17 @@ plugins {
 
 android {
     namespace = "com.example.telnyx_common_example"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
         isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
@@ -28,7 +28,7 @@ android {
         applicationId = "com.example.telnyx_common_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = 30
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
